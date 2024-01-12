1e84bb05a00e69f30dce3c19447e17e53fd3ff6d
데브티브 <thive8564@gmail.com>
2024-01-13 02:49:30 +0900

    test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,13 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { UserModule } from './../src/user/user.module';
+import { AuthModule } from './../src/auth/auth.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, UserModule, AuthModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
